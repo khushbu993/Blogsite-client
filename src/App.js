50e3aabee9b93a17a679e689f743c73dad2bc8c6
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-06-28 13:34:19 +0600

    icon updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import { getPosts } from "./actions/posts";
-import techMemories from "./images/techMemories.png";
+import techMemories from "./images/techMemories.jpg";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from "./styles";
