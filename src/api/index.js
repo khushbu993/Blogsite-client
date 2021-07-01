8e91197e957098c799d312bde467837d99e57348
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-07-02 00:52:22 +0600

    heroku live server link added

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5050" });
+const API = axios.create({
+  baseURL: "https://tech-memories-project.herokuapp.com/",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
